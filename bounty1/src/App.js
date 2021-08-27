a4f257ce9b43e6b68c2aec7fc65525ed81a39f43
BlackBadPinguin <34062375+BlackBadPinguin@users.noreply.github.com>
2021-08-27 23:45:03 +0200

    balance show added

--- bounty1/src/App.js
+++ bounty1/src/App.js
@@ -8,20 +8,68 @@ class LogInWithAlbedo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null
+      value: null,
+      error: null,
+      isLoaded: false,
+      balances: []
     };
   }
+
+  //componentDidMount() {
+  //fetch(
+  // "https://horizon.stellar.org/accounts/GAYOLLLUIZE4DZMBB2ZBKGBUBZLIOYU6XFLW37GBP2VZD3ABNXCW4BVA"
+  //)
+  // .then((res) => res.json())
+  // .then(
+  //  (result) => {
+  //   this.setState({
+  //     isLoaded: true,
+  //    balances: result.balances
+  //  });
+  // },
+  // Note: it's important to handle errors here
+  // instead of a catch() block so that we don't swallow
+  // exceptions from actual bugs in components.
+  //(error) => {
+  //  this.setState({
+  //    isLoaded: true,
+  //    error
+  //   });
+  //   }
+  // );
+  //}
+
   Albedo() {
     albedo.publicKey({}).then((res) => {
       const { intent, pubkey, signature, signed_message } = res;
       console.log({ intent, pubkey, signature, signed_message });
       this.setState((currentState) => {
+        fetch("https://horizon.stellar.org/accounts/" + pubkey)
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                balances: result.balances
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          );
         return { value: pubkey };
       });
     });
   }
+
   render() {
-    const { value } = this.state;
+    const { value, balances } = this.state;
     return (
       <div>
         <div>
@@ -33,8 +81,17 @@ class LogInWithAlbedo extends Component {
           >
             Login With <Image style={{ width: "55px" }} src={albedologo} />
           </Button>
-          <h2>It is {value}.</h2>
+          <div>Public-Key: {value}</div>
         </div>
+        Balances:
+        <ul>
+          {balances.map((item) => (
+            <li key={item.asset_code}>
+              {item.balance} {item.asset_code}
+            </li>
+          ))}
+        </ul>
+        When there is no currency behind the balance, its native XLM.
       </div>
     );
   }
